9bbbe7d99d7b75d19b03c3c65fd5224bb5f8ff07
shardhak <shardhak@gmail.com>
2022-04-22 14:24:17 -0700

    Add project files.

---
+++ DevOpsDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DevOpsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DevOpsDemo.Repository;
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        IPostRepository postRepository;
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
